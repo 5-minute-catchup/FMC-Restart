78b154e705f8769ec2a32b3e61e7efff509642b1
Phoebe Hugh <phoebehugh@gmail.com>
2015-05-23 21:16:18 +0100

    Tidies up ejs file

---
+++ oauth.js
@@ -0,0 +1,9 @@
+var ids = {
+  facebook: {
+   clientID: '653014024831372',
+   clientSecret: '8f7186268d5d2f58856d95c657266f96',
+   callbackURL: 'http://127.0.0.1:1337/auth/facebook/callback'
+  }
+}
+
+module.exports = ids
