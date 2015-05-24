6358cc464f67f3879bb21893ba21ea5f4c9cf3dd
stacca <caronstace@gmail.com>
2015-05-24 14:56:20 +0100

    fixed server

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ var bodyParser = require("body-parser")
 var cookieParser = require("cookie-parser")
 var methodOverride = require('method-override');
 var port = process.env.PORT || 3000
-var io = require('socket.io')(http);
-var http = require('http').Server(app);
 var markers = [];
-var server = require('http').createServer(app);
-var passportStrategy = require('../utils/passport-strategy');
+var passportStrategy = require('./utils/passport-strategy');
+var MongoClient = require('mongodb').MongoClient;
+
+/*add the instance of io here*/
 
 
 var FACEBOOK_APP_ID = "653014024831372";
@@ -37,8 +37,6 @@ var sessionData = session({
   saveUninitialized: true
 });
 
-
-
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
@@ -47,29 +45,37 @@ passport.use(new FacebookStrategy({
   function(accessToken, refreshToken, profile, done) {
 
     process.nextTick(function () {
-      
+
       return done(null, profile);
     });
   }
 ));
 
-
-
-
 var app = express();
 
-  app.set('views', __dirname + '/views');
+  app.set('views', __dirname + '/app/views');
   app.set('view engine', 'ejs');
   app.use(sessionData);
-  app.use(logger());
+  app.use(logger("combined"));
   app.use(cookieParser());
-  app.use(bodyParser());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
   app.use(methodOverride());
-  app.use(session({ secret: 'keyboard cat' }));
-
+  app.use(session({
+      secret: "keyboard cat",
+      saveUninitialized: true, // (default: true)
+      resave: true, // (default: true)
+    }));
   app.use(passport.initialize());
   app.use(passport.session());
-  app.use(express.static(__dirname + '/public'));
+  app.use(express.static(__dirname + '/app/public'));
+  app.use(express.static(__dirname + '/'));
+
+  var http    = require('http');
+      server  = http.createServer(app);
+      io      = require('socket.io')(server);
 
 
 app.get('/', function(req, res){
@@ -84,14 +90,12 @@ app.get('/login', function(req, res){
   res.render('login', { user: req.user });
 });
 
-
 app.get('/auth/facebook',
   passport.authenticate('facebook'),
   function(req, res){
   });
 
-
-app.get('/auth/facebook/callback', 
+app.get('/auth/facebook/callback',
   passport.authenticate('facebook', { failureRedirect: '/login' }),
   function(req, res) {
     res.redirect('/');
@@ -102,9 +106,9 @@ app.get('/logout', function(req, res){
   res.redirect('/');
 });
 
-app.get('/mapjs', function(req, res){
-  res.sendFile(__dirname + '/public/map.js');
-});
+// app.get('/mapjs', function(req, res){
+//   res.sendFile(__dirname + '/app/public/map.js');
+// });
 
 
 // Socket markers start
@@ -126,7 +130,7 @@ io.on('connection', function(socket) {
 
 });
 
-app.listen(port, function(){
+server.listen(port, function(){
   console.log('five minute catch up is on port 3000');
 });
 
